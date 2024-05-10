e212d094acc50b23d4ede4b5b6ddd6e946b8c637
Felipe de Mello Vieira <felipevieirafmv@gmail.com>
2024-05-10 15:02:52 -0300

    deu ruim

--- startup/db.js
+++ startup/db.js
@@ -3,6 +3,6 @@ const config = require('config')
 
 module.exports = function () {
     const db = config.get('db');
-    mongoose.connect(db)
+    mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => console.log(`connected to ${db}`));
 }
